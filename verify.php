48f32fce5bda2a8a9f73ffb566f57120ae972868
Pijitra <31364924+Pijitra@users.noreply.github.com>
2019-01-24 15:52:34 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'IGP1cCGoyG8ki9iUSVs2zFOMjBrgMzXa2ba278mL7jyeDku4U9zCcElEcil+p9q13Crzu6dKzm2f3qvVOQ499g19bNuGVFXmwBTjSqCkFYt/b134CwWlmqtlx2VLmlk4lYWtXRZ8/xWj4oWg9R5I5wdB04t89/1O/w1cDnyilFU=';
+$access_token = 'B6O6gWebXmzLXXF92SgHhkBvCrKw0Aza+N0F0e27yp16/uJFl4EVzESKtD2oM20LvRoaaxGIWcXpLu3r8ZIvEUfjOtF4U0NYbl+rDixshsq2bjOzpfhgcECPEmdYyaOPwrv8rkWz47cquNeLXL79KAdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
